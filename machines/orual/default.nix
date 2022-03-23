85de6368276f247e41318c79b02dc984e558490a
Massimo Redaelli <massimo@typish.io>
2022-03-23 08:18:58 +0100

    orual 2.0

--- machines/orual/default.nix
+++ machines/orual/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./hardware-configuration.nix
+    <nixos-hardware/lenovo/thinkpad/p14s/amd/gen2>
     ../../modules/common_settings.nix
     ../../modules/basic.nix
     ../../modules/wayland.nix
@@ -10,14 +11,29 @@
     ../../modules/bluetooth.nix
   ];
 
+  # erase your darlings
+  environment.etc = {
+    nixos.source = "/persistent/etc/nixos";
+    "NetworkManager/system-connections".source = "/persistent/etc/NetworkManager/system-connections";
+    adjtime.source = "/persistent/etc/adjtime";
+    machine-id.source = "/persistent/etc/machine-id";
+  };
+  systemd.tmpfiles.rules = [
+    "L+ /var/lib/systemd/backlight - - - - /persistent/var/lib/systemd/backlight"
+  ];
+  # users.extraUsers.turing.passwordFile = "/persistent/etc/turing-password";
+  security.sudo.extraConfig = ''
+    # rollback results in sudo lectures after each reboot
+    Defaults lecture = never
+  '';
+
   boot = {
-    loader.grub = {
-      enable = true;
-      version = 2;
-      efiSupport = true;
-      device = "nodev";
-    };
     supportedFilesystems = [ "zfs" ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
   };
 
   networking = {
@@ -25,7 +41,7 @@
       allowedTCPPorts = [ 22 50000 50001 ];
       allowedUDPPorts = [ 50000 ];
     };
-    hostId = "9a88f423";
+    hostId = "5f87931e";
     hostName = "orual";
     networkmanager.enable = true;
   };
@@ -34,7 +50,9 @@
     jp
     rtorrent
     wally-cli
-    keepassxc unstable.innernet ];
+    keepassxc
+    unstable.innernet
+  ];
 
   virtualisation.docker = {
     enable = true;
@@ -45,9 +63,6 @@
   };
 
   services = {
-    xserver = {
-      libinput.enable = true;
-    };
     openssh.enable = true;
     cron = {
       enable = true;
@@ -56,9 +71,12 @@
         "9,19,29,39,49,59 * * * *      turing    . /etc/profile; DISPLAY=:0.0 /home/turing/bin/reminders"
       ];
     };
-    zfs.autoScrub.enable = true;
+    zfs = {
+      autoScrub.enable = true;
+      trim.enable = true;
+    };
     fstrim.enable = true;
   };
 
-  system.stateVersion = "18.09"; # Did you read the comment?
+  system.stateVersion = "21.05";
 }
--- modules/wayland.nix
+++ modules/wayland.nix
@@ -21,6 +21,7 @@
         export _JAVA_AWT_WM_NONREPARENTING=1
         export MOZ_ENABLE_WAYLAND=1
         export XDG_CURRENT_DESKTOP=sway
+        export MOZ_WEBRENDER=1
       '';
       extraPackages = with pkgs; [
         swaylock-fancy
